49cef9808776147a9b0df093a06d18ea5b026076
Theo Henson <theodorehenson@protonmail.com>
2020-06-16 22:13:50 -0400

    Format code

--- server/handler/error.go
+++ server/handler/error.go
@@ -12,5 +12,5 @@ func ErrorHandler(w http.ResponseWriter, r *http.Request, status int) {
 
 	util.Logger.Println("sest-server: http-error: host: " + util.GetRemoteAddr(r) + " error: " + strconv.Itoa(status) + " uri: " + r.RequestURI + " method: " + r.Method + " ua: " + r.UserAgent())
 	w.WriteHeader(status)
-	fmt.Fprint(w, "{\"message:\": \"" + strconv.Itoa(status) + " " + http.StatusText(status) + "\"}")
+	fmt.Fprint(w, "{\"message:\": \""+strconv.Itoa(status)+" "+http.StatusText(status)+"\"}")
 }
